5c1f2e2ffe1fba7044966623cd0c28dc9627a6ff
mac <mac@macs-MacBook-Pro.local>
2020-10-15 08:16:18 +0100

    ipdate

--- Dtos/CreateFixedAssetDto.cs
+++ Dtos/CreateFixedAssetDto.cs
@@ -2,6 +2,35 @@ namespace TaxComputationAPI.Dtos
 {
     public class CreateFixedAssetDto
     {
+        public  int CompanyId  {get;set;}
+
+        public int YearId {get;set;}
+
+        public string MappedCode {get;set;}
+
+        public int AssetId {get;set;}
+
+        public int TriBalanceId {get;set;}
+
+        public bool IsCost {get;set;}
+        public string FixedAssetName {get;set;}
+        public long OpeningCost {get;set;}
         
+        public long CostAddition {get;set;}
+
+        public long CostDisposal {get;set;}
+
+        public long CostClosing {get;set;}
+
+         public long OpeningDepreciation {get;set;}
+        
+        public long DepreciationAddition {get;set;}
+
+        public long DepreciationDisposal {get;set;}
+
+        public long DepreciationClosing {get;set;}
+
     }
+
+    
 }
