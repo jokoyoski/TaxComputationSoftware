2860c1f490c9ea7bbea7c9b73660e82b40328612
oeademola <61713800+oeademola@users.noreply.github.com>
2020-11-17 09:46:45 +0100

    Users stored prod

--- wwwroot/schema_migrations/Users.sql
+++ wwwroot/schema_migrations/Users.sql
@@ -52,7 +52,7 @@ getdate(),
 )
 GO
 
---------------------------------------- STORED PROCEDURE TO  GET USER -----------------------------------------
+--------------------------------------- STORED PROCEDURE TO  GET USER BY EMAIL -----------------------------------------
 IF OBJECT_ID('[dbo].[usp_Get_User_By_Email]') IS NOT NULL
 BEGIN
 DROP procedure [dbo].[usp_Get_User_By_Email]
@@ -66,5 +66,40 @@ AS
 SELECT Email,PasswordHash,PhoneNumber,FirstName,LastName,DateCreated,IsActive from [dbo].[Users] WHERE Email = @Email
 GO
 
+--------------------------------------- STORED PROCEDURE TO  CREATE USERCODES TABLE -----------------------------------------
 
+IF NOT EXISTS(SELECT 1 FROM sysobjects WHERE type = 'U' and name = 'UserCodes')
+BEGIN
+CREATE TABLE UserCodes(
+Id int identity(1,1) NOT NULL,
+Email varchar(50),
+Code varchar(4)
+DateCreated datetime,
+)
+END
+GO
 
+--------------------------------------- STORED PROCEDURE TO  GET USER BY CODE -----------------------------------------
+IF OBJECT_ID('[dbo].[usp_Get_User_By_Code]') IS NOT NULL
+BEGIN
+DROP procedure [dbo].[usp_Get_User_By_Code]
+END
+GO
+CREATE procedure [dbo].[usp_Get_User_By_Code](
+@Code varchar(4)
+)
+AS
+
+SELECT Email,Code,DateCreated from [dbo].[UserCodes] WHERE Code = @Code
+GO
+
+--------------------------------------- STORED PROCEDURE TO  UPDATE PASSWORD -----------------------------------------
+
+IF EXISTS(SELECT FROM Users where Email=@Email)
+BEGIN
+UPDATE [dbo].[Users](
+@PasswordHash varchar(max)
+)
+SET PasswordHash = @PasswordHash
+WHERE Email=@Email
+END
